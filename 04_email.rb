b6b5408206f81e3c36ebe947f77174c56cdef1a1
Marie <marie.goudon@laposte.net>
2019-01-27 21:06:37 +0100

    ajout dun fichier

---
+++ 04_email.rb
@@ -0,0 +1,7 @@
+Mail.deliver do
+  from     'wyffemmuppo-6864@yopmail.com'
+  to       'nakavazum-1149@yopmail.fr'
+  subject  'Here is the image you wanted'
+  body     'This is the text you want to send' 
+  add_file '/full/path/to/somefile.png'
+end
